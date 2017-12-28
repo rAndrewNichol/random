63cdd7aa7615841b30c90346755cf295fd646517
Andrew Nichol <bigboiadmin@Andrews-MBP.centurylink.net>
2017-12-27 22:55:19 -0800

    dfs beta

--- Python/ai/graphs/graph.py
+++ Python/ai/graphs/graph.py
@@ -1,6 +1,6 @@
 #uniform cost graph
 from queue import queue as q
-from stack import stack
+from stack import stack as s
 from collections import defaultdict
 
 class node:
@@ -30,24 +30,25 @@ class graph:
             self.min, self.max = 0, i
         else:
             # this is for graphs instantiated using a list of edges (tuples)
+            maxi = 0
             for edge in self.data:
-                graph[edge[0]].add_edge(edge[1]) 
+                graph[edge[0]].add_edge(edge[1])
+                if edge[1] > maxi: maxi = edge[1]
                 # to accomodate nondirectional edges
                 # graph[edge[1]].add_edge(edge[0])
-            self.min, self.max = min(graph.keys()), max(graph.keys())
+            self.min, self.max = min(graph.keys()), maxi
         return graph
     def find_shortest(self, f = 0, t = 0, method = 'bf'):
         if not t:
             t = self.max
         if not f:
             f = self.min
-        print ("min:{},max:{}".format(self.min,self.max))
         start, goal = self.graph[f], self.graph[t]
         if method not in ['bf','df','a*','dijkstra']:
             raise Exception('invalid method for find_shortest()') 
         if method == 'bf':
             pred = {}
-            explored, queue = set(), q([self.graph[f]])
+            explored, queue = set(), q([start])
             while queue:
                 current = queue.dequeue()
                 explored.add(current)
@@ -62,15 +63,32 @@ class graph:
                         return "->".join(reversed([get_labels[each] for each in path]))
                     pred[out] = current
                     queue.enqueue(out)
-                
+        if method == 'df':
+            shortest = []
+    	    path, stack = [], s([start])
+            while stack:
+                current = stack.pop()
+                path.append(current)
+                for out in [self.graph[each] for each in current.outs if self.graph[each] not in path]:
+                    if out == goal:
+                        if not shortest or len(path) < len(shortest):
+                            shortest, path = path, [start]
+                    else:
+                        stack.push(out)
+            get_labels = {value:str(key) for key, value in self.graph.items()}
+            return "->".join([get_labels[each] for each in (shortest+[goal])])
+               
 g = graph([[0,1,1,0,0],[1,0,0,0,1],[1,0,0,1,0],[0,0,1,0,1],[0,1,0,1,0]])
 gg = graph([(0,1),(0,2),(2,3),(3,4),(1,4)])
 ggg = graph([('A','B'),('A','C'),('C','D'),('D','E'),('B','E')])
-print(gg)
-print(g)
-print (g.find_shortest(0,4,'bf'))
-print (gg.find_shortest(0,4,'bf'))
-print (ggg.find_shortest('A','E','bf'))
-print (g.find_shortest())
-print (gg.find_shortest())
-print (ggg.find_shortest())
+#print (g.find_shortest(0,4,'bf'))
+#print (gg.find_shortest(0,4,'bf'))
+#print (ggg.find_shortest('A','E','bf'))
+##print (g.find_shortest())
+#print (gg.find_shortest())
+#print (ggg.find_shortest())
+
+print (g.find_shortest(0,4,'df'))
+print (gg.find_shortest(0,4,'df'))
+print (ggg.find_shortest('A','E','df'))
+
